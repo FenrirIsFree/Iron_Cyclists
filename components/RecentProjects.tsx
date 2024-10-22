69ceec4f601051012e54d246363158bf9aeedd5b
Nick Lopez <116652056+FenrirIsFree@users.noreply.github.com>
2024-10-22 16:57:07 -0400

    Launch time after ESLint clean up.

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -67,7 +67,7 @@ const RecentProjects = () => {
 
                 <div className="flex justify-center items-center">
                   <p className="flex lg:text-xl md:text-xs text-sm text-purple">
-                    Don't Click Me
+                    Don&apos;t Click Me
                   </p>
                   <FaLocationArrow className="ms-3" color="#CBACF9" />
                 </div>
